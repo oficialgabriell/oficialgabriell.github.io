be4880e850dd55fe9215316b7469e7d26407692e
Gabriel Justino <105810486+oficialgabriell@users.noreply.github.com>
2025-09-15 16:22:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+// Scroll suave já implementado pelo CSS, mas podemos adicionar animações simples
+document.addEventListener('DOMContentLoaded', () => {
+  const cards = document.querySelectorAll('.card');
+  cards.forEach(card => {
+    card.style.opacity = 0;
+  });
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if(entry.isIntersecting){
+        entry.target.style.transition = "opacity 1s, transform 0.8s";
+        entry.target.style.opacity = 1;
+        entry.target.style.transform = "translateY(0)";
+      }
+    });
+  }, { threshold: 0.1 });
+
+  cards.forEach(card => {
+    card.style.transform = "translateY(30px)";
+    observer.observe(card);
+  });
+  
+  particlesJS("particles-js", {
+  particles: {
+    number: { value: 180 },
+    color: { value: "#00b4d8" },
+    size: { 
+      value: 2, // tamanho das partículas
+      random: true // define se o tamanho será aleatório
+    },
+    line_linked: {
+      enable: true,
+      color: "#00b4d8"
+    },
+    move: {
+      enable: true,
+      speed: 2
+    }
+  },
+  interactivity: {
+    events: {
+      onhover: { enable: false, mode: "repulse" },
+      onclick: { enable: false, mode: "push" }
+    }
+  },
+  retina_detect: true
+});
+
+
+});
